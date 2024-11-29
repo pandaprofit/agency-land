0d200bc26dbbf6a896e53a5afddd0545e5e325b1
StepaVam13 <stepavam13@gmail.com>
2024-11-29 19:12:46 +0300

    fix: fix adaptive

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -5,13 +5,9 @@ import classNames from 'classnames'
 
 import styles from './home.module.scss'
 import { HomeProps } from './home.types'
-<<<<<<< HEAD
-import { Intro } from '@modules/intro'
-=======
-import { Intro } from '@/components/intro'
+import { Intro } from '@/modules/intro'
 import { About } from '@/modules/about'
 
->>>>>>> 22932c164a8ecfe04c729c98119d801203181b4e
 
 
 const Home: FC<HomeProps> = ({ className }) => {
