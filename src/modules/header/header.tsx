084d126db1bd097b8d64dcc89e93b93a734cf13d
sitl-idnm <klevtsevitch.lev@yandex.ru>
2025-02-15 18:36:41 +0300

    stranger sins chapter 4

--- src/modules/header/header.tsx
+++ src/modules/header/header.tsx
@@ -3,16 +3,18 @@ import styles from './header.module.scss'
 
 const Header: FC = () => {
 	return (
-		<header className="container mx-auto px-4">
-			<nav>
-				<ul className={styles.nav}>
-					<li>О Нас</li>
-					<li>Портфолио</li>
-					<li>Принципы работы</li>
-					<li>Услуги</li>
-					<li>Контакты</li>
-				</ul>
-			</nav>
+		<header className={styles.header}>
+			<div className="container mx-auto px-4">
+				<nav>
+					<ul className={styles.nav}>
+						<li>О Нас</li>
+						<li>Портфолио</li>
+						<li>Принципы работы</li>
+						<li>Услуги</li>
+						<li>Контакты</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	)
 }
