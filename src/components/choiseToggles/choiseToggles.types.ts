dd1af6b4912e44418639eafe9e0218ed06af3df8
sitl-idnm <klevtsevitch.lev@yandex.ru>
2025-02-18 19:30:34 +0300

    Исправил баг

--- src/components/choiseToggles/choiseToggles.types.ts
+++ src/components/choiseToggles/choiseToggles.types.ts
@@ -1,4 +1,10 @@
+import { Dispatch, RefObject, SetStateAction } from 'react'
+
 export interface ChoiseTogglesProps {
   className?: string
-  onToggleChange?: (toggle: 'cheap' | 'fast' | 'quality' | null) => void
+  activeToggle: 'cheap' | 'fast' | 'quality' | null
+  setActiveToggle: Dispatch<SetStateAction<'cheap' | 'fast' | 'quality' | null>>
+  visibleTooltip: 'cheap' | 'fast' | 'quality' | null
+  setVisibleTooltip: Dispatch<SetStateAction<'cheap' | 'fast' | 'quality' | null>>
+  tooltipRef: RefObject<HTMLDivElement>
 }
